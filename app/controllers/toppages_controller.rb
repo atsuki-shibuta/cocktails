c3b2dc23834c757672604c329690c7df26cb2c15
atsuki.shibuta <shibuta.atsuki@lmi.ne.jp>
2020-06-24 01:55:09 +0000

    toppage_random

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @cocktail = current_user.cocktails.build  # form_with 用
-      @cocktails = current_user.cocktails.order(id: :desc).page(params[:page])
+      @cocktails = Cocktail.order("RAND()").limit(5)
     end
   end
 end
