2dbe76c265dd0294ad3820a1c6c9425b28a5dbe4
atsuki.shibuta <shibuta.atsuki@lmi.ne.jp>
2020-06-25 04:40:07 +0000

    random

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @cocktail = current_user.cocktails.build  # form_with 用
-      @cocktails = Cocktail.order("RANDOM()").limit(5)
+      @cocktails = Cocktail.order("RANDOM()").limit(5)  #mySQLでは"RAND()"、本番は"RANDOM()"
     end
   end
 end
